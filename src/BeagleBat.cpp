a4c5c542f0228c1812a04e7ddc28084d37d87df5
Bradley Saul <bradleysaul@gmail.com>
2016-01-24 20:53:42 -0500

    Initial commit

---
+++ src/BeagleBat.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BeagleBat.cpp
+// Author      : Bradley Saul
+// Version     :
+// Copyright   : copyright 2016
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
